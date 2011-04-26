3445badf54a84076e1f2c52bfcd8418c865df33c
Kiran Bandla <kbandla@in2void.com>
2011-04-26 12:55:09 -0400

    some errors with headers. ex : with ht.ly/. removed.

--- url.py
+++ url.py
@@ -9,18 +9,12 @@ URL Expander
 import sys
 import httplib, urlparse, urllib
 
-headers = {"Accept":"application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5",
-           "Accept-Charset" : "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
-           "Accept-Language" : "en-US,en;q=0.8",
-           "User-Agent" : "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)"
-           }
-
 def main(url):
     if not url.startswith('http'):
         url = "http://"+url
     url = urlparse.urlparse(url)
     conn = httplib.HTTPConnection(url.netloc,80)
-    conn.request("GET",url.path,headers=headers)
+    conn.request("GET",url.path)
     res = conn.getresponse()
     if res.status in [301,307]:
         x = res.getheader("Location")
